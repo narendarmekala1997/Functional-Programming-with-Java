efbff2635438f1dae5cd535359bb88f0480d94da
narender <narendarmekala.9778@gmail.com>
2024-09-13 06:48:09 +0530

    Printing only Even numbers

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -6,15 +6,25 @@ public class FP01Functional {
 	
 	public static void main(String args[]) {
 		
-		printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+		List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+		printAllNumbersInListFunctional(numbers);
+		printEvenNumbersInListFunctional(numbers);
 	}
-	private static void print(int number) {
-		System.out.println(number);
-	}
+	
 	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
 
 		numbers.stream()
-				.forEach(FP01Functional::print);//Method Reference
+				.forEach(System.out::println);//Method Reference
+	}
+	private static boolean isEven(int number) {
+		return number%2==0;
+	}
+	//printing Even numbers
+	private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+
+		numbers.stream()
+				.filter(FP01Functional::isEven)
+				.forEach(System.out::println);//Method Reference
 	}
 
 }
