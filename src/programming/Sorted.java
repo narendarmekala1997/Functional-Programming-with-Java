5d2aafc5b22c6461f6bf35b30ca9b9e9772a87d3
narender <narendarmekala.9778@gmail.com>
2024-09-13 14:00:50 +0530

    Comparator on Sorted method

--- src/programming/Sorted.java
+++ src/programming/Sorted.java
@@ -1,5 +1,6 @@
 package programming;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Sorted {
@@ -7,12 +8,20 @@ public class Sorted {
 	public static void main(String[] args) {
 
 		List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+		List<String> courses = List.of("Spring","Spring Boot","API","Microservices","AWS","PCF","Azure","Docker","Kubernetes");
 		printSorted(numbers);
+		printSortedStrings(courses);
 	}
 	private static void printSorted(List<Integer> numbers) {
 
 		numbers.stream()
 			.sorted()
+			.forEach(System.out::println);
+	}
+	private static void printSortedStrings(List<String> numbers) {
+		numbers.stream()
+			//.sorted(Comparator.reverseOrder())
+			.sorted(Comparator.comparing( str -> str.length()))
 			.forEach(System.out::println);
 	}
 	
