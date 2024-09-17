2ee131ffed0762bfb374c843515fe425a7c811da
narender <narendarmekala.9778@gmail.com>
2024-09-17 22:42:52 +0530

    iterate range and boxed methods

--- src/programming/Creating_Streams.java
+++ src/programming/Creating_Streams.java
@@ -1,6 +1,7 @@
 package programming;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Creating_Streams {
@@ -22,6 +23,12 @@ public class Creating_Streams {
     System.out.println(Arrays.stream(numberArray).sum());
     System.out.println(Arrays.stream(numberArray).min());
     System.out.println(Arrays.stream(numberArray).max());
+    
+    
+   System.out.println( IntStream.range(1, 10).sum());
+   System.out.println( IntStream.rangeClosed(1, 10).sum());
+   System.out.println( IntStream.iterate(0, e->e+2).peek(System.out::println).limit(10).sum());
+   System.out.println( IntStream.iterate(2, e->e*2).peek(System.out::println).limit(10).sum());
     
 	
 	}
